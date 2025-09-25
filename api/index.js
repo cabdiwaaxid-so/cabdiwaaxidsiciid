39e5e28eefd47873cd54275b7e2c617cf6a2e11f
Cabdi Waaxid Siciid <158821136+cabdiwaaxid-so@users.noreply.github.com>
2025-09-25 06:23:36 +0300

    Add files via upload

--- api/index.js
+++ api/index.js
@@ -3,22 +3,18 @@ const app = new novax();
 app.serveStatic()
 const redirectMiddleware = (req, res, next) => {
   const originalUrl = req.fullUrl || `${req.protocol}://${req.headers.host}${req.url}`;
-  const targetDomain = 'https://www.cabdiwaaxidsiciid.site/';
+  const targetDomain = `https://www.cabdiwaaxidsiciid.site${req.url}`;
   
   if (originalUrl === 'https://cabdiwaaxidsiciid.vercel.app/' || originalUrl.includes('cabdiwaaxidsiciid.vercel.app')) {
-    
-    const newUrl = originalUrl.replace('https://cabdiwaaxidsiciid.vercel.app', 'https://www.cabdiwaaxidsiciid.site');
-    
     res.set({
       'X-Redirect-By': 'Novaxjs2-Redirect-Middleware',
-      'X-New-Location': newUrl
+      'X-New-Location': targetDomain
     });
-    return res.redirect(newUrl, 301);
+    return res.redirect(targetDomain, 301);
   }
   next();
 };
-
 app.useMiddleware(redirectMiddleware);
-app.get('/', (req, res) => app.sendFile('./index.html', res));
-app.on(404, (req, res) => app.sendFile('./404.html', res));
+app.get('/', (req, res) => app.sendFile('./public/index.html', res));
+app.on(404, (err, req, res) => app.sendFile('./public/404.html', res));
 app.at(3000, '0.0.0.0', () => console.log('Server is running on port 3000'));
